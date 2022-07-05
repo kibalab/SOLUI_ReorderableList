ce58d4be90114f79628b86b3468448c8406d9a7d
KIBA <kjh030529@gmail.com>
2022-07-05 11:09:31 +0900

    Create Element Add Remove And Element Render Functions

--- Assets/Scripts/RuntimeReorderableList.cs
+++ Assets/Scripts/RuntimeReorderableList.cs
@@ -1,8 +1,75 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using Unity.VisualScripting;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
+
+[ExecuteAlways]
 public class RuntimeReorderableList : MonoBehaviour
 {
-    public List<RectTransform> elements;
+    private ObservableCollection<object> elements;
+
+    private Dictionary<object, RectTransform> elementTransforms;
+
+    public RectTransform ElementObject;
+
+    public RectTransform ContentsPanel;
+
+    public bool add = false;
+
+    public void Update()
+    {
+        if (add)
+        {
+            add = false;
+            if(elements != null) elements.Add(Random.value);
+            else elements = new ObservableCollection<object>() { Random.value };
+        }
+    }
+
+    private void Start()
+    {
+        elements = new ObservableCollection<object>();
+        elementTransforms = new Dictionary<object, RectTransform>();
+        elements.CollectionChanged += OnListChanged;
+    }
+
+    public void OnListChanged(object sender, NotifyCollectionChangedEventArgs args)
+    {
+        switch (args.Action)
+        {
+            case NotifyCollectionChangedAction.Add :
+                foreach (var item in args.NewItems)
+                {
+                    elementTransforms.Add(item, SpawnElement());
+                }
+                break;
+            case NotifyCollectionChangedAction.Remove :
+                foreach (var item in args.NewItems)
+                {
+                    elementTransforms.Remove(item);
+                }
+                break;
+            case NotifyCollectionChangedAction.Replace :
+                break;
+            case NotifyCollectionChangedAction.Reset :
+                break;
+        }
+        UpdatePanelScale();
+    }
+
+    public RectTransform SpawnElement()
+    {
+        var Element = Instantiate(ElementObject, ContentsPanel);
+        Element.anchoredPosition = new Vector2(0,-ListLastPos);
+        return Element;
+    }
+
+    public void UpdatePanelScale() => ContentsPanel.sizeDelta = new Vector2(0, ListLastPos);
+
+    public float ListLastPos { get => ElementObject.sizeDelta.y * elements.Count - 1; }
 }
